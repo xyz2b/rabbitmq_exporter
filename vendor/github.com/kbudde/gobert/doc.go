ce19edefe31c81257054fcbb4d1bc6a81e00ba63
Kris Budde <Kris.Budde@gmail.com>
2017-12-01 06:27:15 +0100

    use dep as dependency management tool

--- vendor/github.com/kbudde/gobert/doc.go
+++
@@ -1,5 +0,0 @@
-// Package bert implements the BERT serialization and RPC protocol.
-// See http://bert-rpc.org/
-package bert
-
-// BUG(josh): Full BERT specification compliance is still in progress.
