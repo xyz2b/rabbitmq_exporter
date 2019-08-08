e57b9631e16e38684161e2449e56696f682af84f
Kris Budde <kris.budde@gmail.com>
2019-08-08 21:04:55 +0200

    handle os.interrupt and stop server #137

--- service.go
+++ service.go
@@ -2,7 +2,22 @@
 
 package main
 
-//runService dummy for non windows systems
+import (
+	"os"
+	"os/signal"
+)
+
+//runService wait for os interrupt
 func runService() chan bool {
-	return make(chan bool)
+	waitChan := make(chan bool)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			waitChan <- true
+			return
+		}
+	}()
+	return waitChan
 }
