8acfbe43c57d029f7d20a8b42a970989c7963a29
Kris Budde <Budde@skycom.com>
2020-04-20 21:56:21 +0200

    handle more signals (gracefully docker shutdown) # 137

--- service.go
+++ service.go
@@ -5,6 +5,7 @@ package main
 import (
 	"os"
 	"os/signal"
+	"syscall"
 )
 
 //runService wait for os interrupt
@@ -12,7 +13,7 @@ func runService() chan bool {
 	waitChan := make(chan bool)
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		for range c {
 			waitChan <- true
